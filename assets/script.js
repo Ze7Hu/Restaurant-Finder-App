23779e3f9792c62205d94a196e40008339cfe845
Abdishakur Abdalla Gesey <karbuuno@gmail.com>
2023-03-15 19:47:25 +0000

    localStorage and rating stars

--- assets/script.js
+++ assets/script.js
@@ -1,79 +1,72 @@
 var searchInput = document.getElementById("search-input");
 var btnSearch = document.getElementById("btn-search");
 var restaurant = document.getElementById("restaurant");
+var btnRefresh = document.getElementById("btn-refresh");
+var checkbox = document.getElementById("checkbox");
+var rating = document.getElementById("rating");
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "53b514667bmsh143295e0a8819afp138c99jsn88f905128703",
+//     "X-RapidAPI-Host": "wyre-data.p.rapidapi.com",
+//   },
+// };
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": 'ca7062ca52msh72839442beb59d9p15247ajsnde42fd9c86c9',
-    "X-RapidAPI-Host": "wyre-data.p.rapidapi.com",
-  },
-};
+////////////////////////////////////////////////////////////////
+//
+var newRestaurant = [];
+// getting data from localStorage//
+function getLocalStorage() {
+  var storageData = JSON.parse(window.localStorage.getItem("data")) || [];
+  displayList(storageData);
+}
+/// using while loop to get 6 of 28 restaurants
+var arr = [];
+while (arr.length < 6) {
+  var r = Math.floor(Math.random() * 28) + 1;
+  if (arr.indexOf(r) === -1) arr.push(r);
+}
 
 function getRestaurant() {
-  fetch(
-    `https://wyre-data.p.rapidapi.com/restaurants/town/${searchInput.value}`,
-    options
-  )
+  //The API key doesn't work any more. So I used to save in JSON file to used it.
+  fetch(`../data.json`)
     .then(response => response.json())
     .then(function (data) {
-      var arrData = Object.entries(data);
-      // console.log(arrData);
-      for (var i = 0; i < arrData.length; i++) {
-        var randomRestaurants =
-          arrData[Math.floor(Math.random() * arrData.length)];
-        console.log(randomRestaurants[i]);
-        restaurant.innerHTML = `
-        <div id="restaurant" class="leading-8 text-2xl w-72">
-        <h3 font-bold>Name: ${randomRestaurants[i].BusinessName}</h3>
-                <p>Addresses: ${randomRestaurants[i].AddressLine2}.</p>
-                <p>Area: ${randomRestaurants[i].AddressLine3}.</p>
-                <p>Postcode: ${randomRestaurants[i].PostCode}.</p>
-                <p>Stars: ${randomRestaurants[i].RatingValue}.</p>
+      console.log(data);
 
-    
-        `;
+      for (var i = 0; i <= arr.length - 1; i++) {
+        console.log("i", arr);
+        newRestaurant.push({
+          BusinessName: data[arr[i]]["BusinessName"],
+          Address: data[arr[i]]["AddressLine2"],
+          Area: data[arr[i]]["AddressLine3"],
+          Postcode: data[arr[i]]["PostCode"],
+          Stars: data[arr[i]]["RatingValue"],
+        });
       }
+
+      // saving data to the localStorage
+      window.localStorage.setItem("data", JSON.stringify(newRestaurant));
+      displayList(newRestaurant);
     })
     .catch(err => console.error(err));
 }
 btnSearch.addEventListener("click", function () {
   getRestaurant();
+
   console.log("clicked");
 });
-
-
-
-/*function logData() {
-	console.log(getData());
+// displaying data
+function displayList(list) {
+  restaurant.innerHTML = "";
+  list.forEach(rs => {
+    restaurant.innerHTML += `
+    <div id="restaurant" class=" restaurant leading-8 text-2xl w-72 bg-gray-100 rounded-sm p-4  ">
+    <h3 font-bold>Name: ${rs.BusinessName}</h3>
+            <p>Addresses: ${rs.Address}.</p>
+            <p>Area: ${rs.Area}.</p>
+            <p>Postcode: ${rs.Postcode}.</p>
+            <p >Stars:${"⭐".repeat(rs.Stars)} </p>`;
+  });
 }
-
-function getData() {
-	return {
-		liked: localStorage.getItem('liked') ? localStorage.getItem('liked') : "",
-		unliked: localStorage.getItem('unliked') ? localStorage.getItem('unliked') : ""
-	};
-}
-getData();
-
-
-function refreshPage(){
-  window.location.reload();
-} 
-<button type="submit" onClick="refreshPage()">Refresh Button</button> */
-
-
-
-
-/*const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'ca7062ca52msh72839442beb59d9p15247ajsnde42fd9c86c9',
-		'X-RapidAPI-Host': 'wyre-data.p.rapidapi.com'
-	}
-};
-
-fetch('https://wyre-data.p.rapidapi.com/restaurants/town/hambleton', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err)); */
+getLocalStorage();
